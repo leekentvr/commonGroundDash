53245cc0f85a42d2b136ad5e8d36a36c80f4f118
leekentvr <leedakent123@hotmail.com>
2026-02-27 15:48:01 +0900

    use the correct local python

--- cgdashboard.bat
+++ cgdashboard.bat
@@ -1,8 +1,16 @@
 @echo off
 cd /d "%~dp0"
 
-python "%~dp0main.py"
+REM Use venv Python explicitly
+set "PYTHON_EXE=%~dp0venv\Scripts\python.exe"
 
+if not exist "%PYTHON_EXE%" (
+    echo ERROR: venv Python not found at %PYTHON_EXE%
+    pause
+    exit /b 1
+)
+
+"%PYTHON_EXE%" "%~dp0main.py"
 echo.
 echo Script finished.
 pause
